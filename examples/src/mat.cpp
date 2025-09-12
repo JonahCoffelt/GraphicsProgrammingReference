d8328a4a3f374f487d71e419bb77ec5514d9045c
Jonah Coffelt <coffelt.jonah0918@gmail.com>
2025-09-12 01:15:59 -0500

    Added docs to mat.cpp

--- examples/src/mat.cpp
+++ examples/src/mat.cpp
@@ -16,17 +16,30 @@ void Matrix::makeIdentity() {
     matrix = glm::mat4(1.0f);
 }
 
-
+/**
+ * @brief Translates this matrix by the given vector
+ * 
+ * @param translation Vector of translation (x, y, z)
+ */
 void Matrix::translate(glm::vec3 translation) {
     matrix = glm::translate(matrix, translation);
 }
 
-
+/**
+ * @brief Rotates this matrix along the given axis for 'rotation' radians
+ * 
+ * @param axis Axis about which to rotate
+ * @param rotation Magnitude of rotation in radians
+ */
 void Matrix::rotate(glm::vec3 axis, float rotation) {
     matrix = glm::rotate(matrix, rotation, axis);
 }
 
-
+/**
+ * @brief Scales this matrix by the given vector
+ * 
+ * @param scale Vector of scale (x, y, z)
+ */
 void Matrix::scale(glm::vec3 scale) {
     matrix = glm::scale(matrix, scale);
 }
